7cec55a031eef08b6c98e6df0ad9e3fdc4a9d4a4
ivddorrka <ivddorrka@gmail.com>
2023-05-23 23:11:39 +0200

    documentation added

--- paint_steps.m
+++ paint_steps.m
@@ -1,3 +1,24 @@
+%%%%
+% Function paint_steps is a helper function, to decide what to do with
+% an object coordinate in case it is out of boundaries AND depending on
+% the type of the wall-type
+
+% ACCEPTS:
+% * c - boolean - if true - than the x-coordinate is stable, and object has
+% moved along y-coordinate, vice-versa otherwise
+% * c_stable - coordinate of a stable coordinate
+% * c_moving_start - coordinate-start for a moving 
+% * c_moving_end - coordinate-end for a moving 
+% * color - color to mark the trace
+% * field - current field to make trace on
+% * was_reflected - boolean: whether object was reflected through the wall or not
+% * fieldSize - size of the current field
+
+% RETURNS:
+% field - updated field with colored steps
+
+
+
 function [field] = paint_steps(c, c_stable, c_moving_start, c_moving_end, color, field, was_reflected, fieldSize)
    
     if ~was_reflected || (abs(c_moving_end-c_moving_start)<=10 && ~was_reflected)
